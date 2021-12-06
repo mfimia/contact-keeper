198239734bb14c71ccd0eccac491e80960a4f2b8
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-06 09:40:30 +0100

    set up route to get all user's contacts

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,15 +1,30 @@
 const express = require("express");
 const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const auth = require("../middleware/auth");
 
+const User = require("../models/User");
+const Contact = require("../models/Contact");
+
+// This route allows us to get all contacts from an user
 // @route   GET api/contacts
 // desc     Get all user contacts
 // @access  Private
-router.get("/", (req, res) => {
-  res.send("Get all contacts");
+router.get("/", auth, async (req, res) => {
+  try {
+    // We get the contact info once it has been verified by middleware and we then sort it by closest date
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
 });
 
 // @route   POST api/contacts
-// desc     Get all user contacts
+// desc     Add contact
 // @access  Private
 router.post("/", (req, res) => {
   res.send("Add contact");
