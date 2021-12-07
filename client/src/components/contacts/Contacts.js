de3f77a6a91317c725cb1cbe9ccd165192233b87
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-07 15:43:50 +0100

    method to filter contacts inserted in the UI

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -4,13 +4,26 @@ import ContactItem from "./ContactItem";
 
 const Contacts = () => {
   const contactContext = useContext(ContactContext);
+  // If there are no contacts, we tell the user to please add
+  const { contacts, filtered } = contactContext;
 
-  const { contacts } = contactContext;
+  if (contacts.length === 0) {
+    return <h4>Please add a contact</h4>;
+  }
+
+  // We are pulling the filtered items from context
+
   return (
     <Fragment>
-      {contacts.map((contact) => (
-        <ContactItem contact={contact} key={contact.id} />
-      ))}
+      {/* If filtered has a value, then we map the contacts based on the filtered list. 
+      If filtered has no  value, we just map the contacts through the normal contact list */}
+      {filtered
+        ? filtered.map((contact) => (
+            <ContactItem key={contact.id} contact={contact} />
+          ))
+        : contacts.map((contact) => (
+            <ContactItem contact={contact} key={contact.id} />
+          ))}
     </Fragment>
   );
 };
