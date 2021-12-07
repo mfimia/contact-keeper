9935c3c9502abaccc666d7acab52eaba044f70ee
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-07 21:06:39 +0100

    set up alert context and added alert to register page. all fields are required and alert is displayed if passwords dont match

---
+++ client/src/context/alert/AlertState.js
@@ -0,0 +1,34 @@
+import { useReducer } from "react";
+import AlertContext from "./AlertContext";
+import AlertReducer from "./AlertReducer";
+import { SET_ALERT, REMOVE_ALERT } from "../types";
+
+const AlertState = (props) => {
+  const initialState = [];
+
+  const [state, dispatch] = useReducer(AlertReducer, initialState);
+
+  // Set Alert
+  const setAlert = (msg, type, timeout = 3000) => {
+    const id = Math.floor(Math.random() * 1000000);
+    dispatch({
+      type: SET_ALERT,
+      payload: { msg, type, id },
+    });
+
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
+  };
+
+  return (
+    <AlertContext.Provider
+      value={{
+        alerts: state,
+        setAlert,
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
+  );
+};
+
+export default AlertState;
