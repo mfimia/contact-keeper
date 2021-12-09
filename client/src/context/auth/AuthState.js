24ba2b716d2976f1d6aa343816cc09e69dceed8f
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-09 15:04:48 +0100

    prepare for deploy

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -33,7 +33,7 @@ const AuthState = (props) => {
       setAuthToken(localStorage.getItem("token"));
     }
     try {
-      const res = await axios.get("http://localhost:5000/api/auth");
+      const res = await axios.get("/api/auth");
       dispatch({ type: USER_LOADED, payload: res.data });
     } catch (err) {
       dispatch({ type: AUTH_ERROR });
@@ -49,11 +49,7 @@ const AuthState = (props) => {
     };
 
     try {
-      const res = await axios.post(
-        "http://localhost:5000/api/users",
-        formData,
-        config
-      );
+      const res = await axios.post("/api/users", formData, config);
 
       dispatch({
         type: REGISTER_SUCCESS,
@@ -82,11 +78,7 @@ const AuthState = (props) => {
     };
     // We send a request to the server and wait for the response
     try {
-      const res = await axios.post(
-        "http://localhost:5000/api/auth",
-        formData,
-        config
-      );
+      const res = await axios.post("/api/auth", formData, config);
       // Once the response is there, we execute the action in our state
       dispatch({
         type: LOGIN_SUCCESS,
