bfaadf936e12223d4df20cdf319b1477d21e825a
Martin Fimia <martin.fimia.osuna@hotmail.com>
2021-12-09 08:13:14 +0100

    set up logout method

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,12 +8,14 @@ const Navbar = ({ title, icon }) => {
 
   const { isAuthenticated, logout, user } = authContext;
 
+  const onLogout = () => logout();
+
   const authLinks = (
     <Fragment>
       {/* If the user exists, the app says hi */}
       <li>Hello {user && user.name}</li>
       <li>
-        <a href="#!">
+        <a onClick={onLogout} href="#!">
           <i className="fas fa-sign-out-alt"></i>
           <span className="hide-sm">Logout</span>
         </a>
